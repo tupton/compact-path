cdd258b512b94ba8ef918833a8ddb758cd13786b
Thomas Upton <thomasupton@gmail.com>
2022-08-25 12:27:30 -0500

    Migrate from nose to pytest

--- test_compact_path.py
+++ test_compact_path.py
@@ -1,46 +1,44 @@
 from compact_path import compact_path
 
-import nose.tools as tools
-
 def test_empty():
-    tools.eq_('', compact_path(''))
+    assert '' == compact_path('')
 
-    tools.eq_(None, compact_path(None))
+    assert None == compact_path(None)
 
 def test_root():
-    tools.eq_('/', compact_path('/'))
+    assert '/' == compact_path('/')
 
 def test_absolute():
-    tools.eq_('/usr', compact_path('/usr'))
+    assert '/usr' == compact_path('/usr')
 
-    tools.eq_('/u/local', compact_path('/usr/local'))
+    assert '/u/local' == compact_path('/usr/local')
 
-    tools.eq_('/u/l/bin', compact_path('/usr/local/bin'))
+    assert '/u/l/bin' == compact_path('/usr/local/bin')
 
 def test_relative():
-    tools.eq_('usr', compact_path('usr'))
+    assert 'usr' == compact_path('usr')
 
-    tools.eq_('u/local', compact_path('usr/local'))
+    assert 'u/local' == compact_path('usr/local')
 
 def test_home():
-    tools.eq_('~', compact_path('~'))
+    assert '~' == compact_path('~')
 
-    tools.eq_('~/code', compact_path('~/code'))
+    assert '~/code' == compact_path('~/code')
 
-    tools.eq_('~/c/compact_path', compact_path('~/code/compact_path'))
+    assert '~/c/compact_path' == compact_path('~/code/compact_path')
 
 def test_max_length():
     path = '/usr/local/bin'
-    tools.eq_(path, compact_path(path, len(path)))
+    assert path == compact_path(path, len(path))
 
-    tools.eq_('/u/l/bin', compact_path(path, trigger=1))
+    assert '/u/l/bin' == compact_path(path, trigger=1)
 
 def test_trailing_slash():
-    tools.eq_('/u/l/bin', compact_path('/usr/local/bin/'))
+    assert '/u/l/bin' == compact_path('/usr/local/bin/')
 
 def test_spaces():
-    tools.eq_("/u/l/b in", compact_path("/usr/lo cal/b in"))
+    assert "/u/l/b in" == compact_path("/usr/lo cal/b in")
 
-    tools.eq_("/u/l/b in", compact_path("/usr/ lo cal/b in"))
+    assert "/u/l/b in" == compact_path("/usr/ lo cal/b in")
 
-    tools.eq_("/u/l/ bin", compact_path("/usr/ lo cal/ bin"))
+    assert "/u/l/ bin" == compact_path("/usr/ lo cal/ bin")
